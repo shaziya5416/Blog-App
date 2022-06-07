477d47e90d0e109b35c67ab3e5d16070fafeb7fb
Shazia Khan <shaziya5416@gmail.com>
2022-06-07 16:30:49 +0530

    Day1

---
+++ blog-app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./stylesheet/style.css";
+import App from "./components/App.js";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
